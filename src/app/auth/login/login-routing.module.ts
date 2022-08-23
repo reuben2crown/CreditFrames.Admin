6443b15e760c79e88e9d3c5e7bbea425edde4725
Reuben A. Keye <reuben2crown@yahoo.co.uk>
2022-08-23 21:25:45 +0100

    login title

--- src/app/auth/login/login-routing.module.ts
+++ src/app/auth/login/login-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login.component';
 
-const routes: Routes = [{ path: '', component: LoginComponent }];
+const routes: Routes = [{ path: '', component: LoginComponent, data: { title: 'Login' } }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
